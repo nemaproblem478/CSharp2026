9df034757ad21f92c3ac8d7d671afcc0b22ec279
nemaproblem478 <danylosavchuk0102@gmail.com>
2026-02-15 17:46:17 +0200

    Change name of DB models folder and namespace. Get rid of Enums localy

---
+++ ProductManager.DBModels/ProductDBModel.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductManager.ProductManagerDBModels
+{
+    public class ProductDBModel
+    {
+        private Guid _productId;
+        private Guid _warehouseId;
+        private string _name;
+        private int _quantity;
+        private double _price;
+        private Category _category;
+        private string _description;
+        private double _totalCost;
+
+        public Guid ProductId { get => _productId; }
+        public Guid WarehouseId { get => _warehouseId; set => _warehouseId = value; }
+        public string Name { get => _name; set => _name = value; }
+        public int Quantity { get => _quantity; set => _quantity = value; }
+        public double Price { get => _price; set => _price = value; }
+        public Category Category { get => _category; set => _category = value; }
+        public string Description { get => _description; set => _description = value; }
+        public double TotalCost { get => _totalCost; }
+
+        private ProductDBModel() 
+        {
+
+        }
+        public ProductDBModel(Guid warehouseId, string name, int quantity, double price, Category category, string description)
+        {
+            _productId = Guid.NewGuid();
+            _warehouseId = warehouseId;
+            _name = name;
+            _quantity = quantity;
+            _price = price;
+            _category = category;
+            _description = description;
+        }
+    }
+}
---
+++ ProductManager.DBModels/WarehouseDBModel.cs
@@ -0,0 +1,31 @@
+﻿
+namespace ProductManager.DBModels
+{
+    public class WarehouseDBModel
+    {
+        private Guid _id;
+        private string _name;
+        private Location _location;
+        private List<ProductDBModel> _products;
+        private double _totalCost;
+
+        public Guid Id { get => _id; }
+        public string Name { get => _name; set => _name = value; }
+        public Location Location { get => _location; set => _location = value; }
+        public List<ProductDBModel> Products { get => _products; set => _products = value; }
+        public double TotalCost { get => _totalCost; }
+
+        private WarehouseDBModel()
+        {
+
+        }
+        public WarehouseDBModel(string name, Location location, List<ProductDBModel> products)
+        {
+            _id = Guid.NewGuid();
+            _name = name;
+            _location = location;
+            _products = products;
+        }
+
+    }
+}
