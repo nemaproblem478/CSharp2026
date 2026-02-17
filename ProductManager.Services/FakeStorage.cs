fa2af8226d21344ed73c3fb98590086b850cdfdd
nemaproblem478 <danylosavchuk0102@gmail.com>
2026-02-17 18:07:15 +0200

    Add fake storage implementation and service

---
+++ ProductManager.Services/FakeStorage.cs
@@ -0,0 +1,41 @@
+﻿using ProductManager.DBModels;
+using ProductManager.CommonComponents;
+
+namespace ProductManager.Services
+{
+    internal static class FakeStorage
+    {
+        private static readonly List<WarehouseDBModel> _warehouses;
+        private static readonly List<ProductDBModel> _products;
+
+        internal static IEnumerable<WarehouseDBModel> Warehouses
+        {
+            get
+            {
+                return _warehouses.ToList();
+            }
+        }
+        internal static IEnumerable<ProductDBModel> Products
+        {
+            get
+            {
+                return _products.ToList();
+            }
+        }
+
+        static FakeStorage()
+        {
+            var warehouseKyiv = new WarehouseDBModel("Kyiv Warehouse", Location.Kyiv);
+            var warehouseKhmelnytsky = new WarehouseDBModel("Khmelnytsky Warehouse", Location.Khmelnytsky);
+            var warehouseLviv = new WarehouseDBModel("Lviv Warehouse", Location.Lviv);
+            _warehouses = new List<WarehouseDBModel> { warehouseKyiv, warehouseKhmelnytsky, warehouseLviv };
+            _products = new List<ProductDBModel>
+            {
+                new ProductDBModel(warehouseKyiv.Id, "1993 Fender Telecaster TC72", 1, 1500, Category.ElectricGuitars, "1993 Fender Japan '72 Telecaster Custom (TC72) in Ocean Turquoise Metallic (OTM)—the rare early FujiGen iteration of the color, which is much darker than the common CIJ iteration."),
+                new ProductDBModel(warehouseKhmelnytsky.Id, "Marshall JCM 900", 2, 1150, Category.Amplifiers, "Marshall JCM 900 model 4102 amp. 2 X G12T-75 Speakers. 100watt combo. Recently serviced. Great Sound"),
+
+            };
+
+        }
+    }
+}
--- ProductManager.Services/StorageService.cs
+++ ProductManager.Services/StorageService.cs
@@ -1,13 +1,44 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using ProductManager.DBModels;
+using ProductManager.Services;
 
 namespace ProductManager.ProductManager.Services
 {
     public class StorageService
     {
+        private List<WarehouseDBModel> _warehouses;
+        private List<ProductDBModel> _products;
 
+        public void LoadData()
+        {
+            if (_warehouses != null && _products != null)
+                return;
+            _warehouses = FakeStorage.Warehouses.ToList();
+            _products = FakeStorage.Products.ToList();
+        }
+
+        public IEnumerable<WarehouseDBModel> GetWarehouses()
+        {
+            LoadData();
+            var resultList = new List<WarehouseDBModel>();
+            foreach (var warehouse in _warehouses)
+            {
+                resultList.Add(warehouse);
+            }
+            return resultList;
+        }
+        public IEnumerable<ProductDBModel> GetProducts(Guid warehouseId)
+        {
+            LoadData();
+            var resultList = new List<ProductDBModel>();
+            foreach(var product in _products)
+            {
+                if (product.WarehouseId == warehouseId)
+                {
+                    resultList.Add(product);
+                }
+            }
+            return resultList;
+        }
     }
+
 }
