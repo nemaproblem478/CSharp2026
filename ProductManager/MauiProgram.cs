3087aa3b8da588027cb5c7762d731bd294690074
nemaproblem478 <danylosavchuk0102@gmail.com>
2026-03-19 10:55:39 +0200

    Dependencies update for other files

--- ProductManager/MauiProgram.cs
+++ ProductManager/MauiProgram.cs
@@ -1,6 +1,9 @@
 ﻿using Microsoft.Extensions.Logging;
 using ProductManager.Pages;
 using ProductManager.Services;
+using ProductManager.Repository;
+using ProductManager.Storage;
+using ProductManager.Viewmodels;
 
 namespace ProductManager
 {
@@ -17,16 +20,21 @@ namespace ProductManager
                     fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
                 });
 
-            builder.Services.AddSingleton<IStorageService, StorageService>();
-            builder.Services.AddTransient<IWarehouseService, WarehouseService>();
-            builder.Services.AddTransient<IProductService, ProductService>();
+            builder.Services.AddSingleton<IStorageContext, InMemoryStorage>();
+            builder.Services.AddSingleton<IWarehouseRepository, WarehouseRepository>();
+            builder.Services.AddSingleton<IProductRepository, ProductRepository>();
+            builder.Services.AddSingleton<IWarehouseService, WarehouseService>();
+            builder.Services.AddSingleton<IProductService, ProductService>();
 
-            builder.Services.AddTransient<WarehousesPage>();
+            builder.Services.AddSingleton<WarehousesPage>();
             builder.Services.AddTransient<WarehouseDetailsPage>();
             builder.Services.AddTransient<ProductDetailsPage>();
-            builder.Services.AddTransient<ProductCreatePage>();
+
+            builder.Services.AddSingleton<WarehousesViewModel>();
+            builder.Services.AddTransient<WarehouseDetailsViewModel>();
+            builder.Services.AddTransient<ProductDetailsViewModel>();
 #if DEBUG
-    		builder.Logging.AddDebug();
+            builder.Logging.AddDebug();
 #endif
 
             return builder.Build();
