2211365cab75dfb22fc6d7d0ee709b2749ba67e7
nemaproblem478 <danylosavchuk0102@gmail.com>
2026-03-19 10:53:04 +0200

    Update WarehouseDetailsPage to MVVM

--- ProductManager/Pages/WarehouseDetailsPage.xaml.cs
+++ ProductManager/Pages/WarehouseDetailsPage.xaml.cs
@@ -1,56 +1,14 @@
 using ProductManager.Services;
 using ProductManager.UIModels;
+using ProductManager.Viewmodels;
 using System.Collections.ObjectModel;
 
 namespace ProductManager.Pages;
-
-[QueryProperty(nameof(CurrentWarehouse), "SelectedWarehouse")]
 public partial class WarehouseDetailsPage : ContentPage
 {
-	private WarehouseUIModel _currentWarehouse = null!;
-    private readonly IProductService _service;
-    public WarehouseUIModel CurrentWarehouse
+	public WarehouseDetailsPage(WarehouseDetailsViewModel vm)
 	{
-		get => _currentWarehouse;
-		set
-		{
-			_currentWarehouse = value;
-			BindingContext = CurrentWarehouse;
-		}
-	}
-	public WarehouseDetailsPage(IProductService service)
-	{
-		_service = service;
         InitializeComponent();
-
-		
-    }
-    //Handling selection of a product and navigating into the ProductDetailsPage
-    public void ProductSelected(object sender, SelectionChangedEventArgs e)
-	{
-		var product = (ProductUIModel)e.CurrentSelection[0];
-		Shell.Current.GoToAsync($"{nameof(ProductDetailsPage)}", new Dictionary<string, object> { { "SelectedProduct", product } });
-	}
-	//Handling navigating to ProductCreatePage when clicking on "Create" button
-	private void CreateClicked(object sender, EventArgs e)
-	{
-        Shell.Current.GoToAsync($"{nameof(ProductCreatePage)}", new Dictionary<string, object> { { "CurrentWarehouse", _currentWarehouse.Id } });
-    }
-    //Handling updating information of all products on appearing
-    protected override void OnAppearing()
-    {
-        base.OnAppearing();
-
-        if (_currentWarehouse != null)
-        {
-            var data = _service.GetProductsUI(_currentWarehouse.Id).ToList();
-
-			CurrentWarehouse.Products = new ObservableCollection<ProductUIModel>(data);
-
-            var ContextUi = new WarehouseUIModel(_currentWarehouse);
-			ContextUi.Products = new ObservableCollection<ProductUIModel>(data);
-
-			BindingContext = ContextUi;
-        }
+        BindingContext = vm;
     }
 }
