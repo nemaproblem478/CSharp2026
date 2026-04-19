ce5c0d7adf07413dacbfe65acbd56bc3d911ac87
nemaproblem478 <danylosavchuk0102@gmail.com>
2026-04-19 15:16:09 +0300

    Update AppShell routing

--- ProductManager/AppShell.xaml.cs
+++ ProductManager/AppShell.xaml.cs
@@ -8,7 +8,8 @@ namespace ProductManager
         {
             InitializeComponent();
             Routing.RegisterRoute($"{nameof(WarehousesPage)}/{nameof(WarehouseDetailsPage)}", typeof(WarehouseDetailsPage));
-            Routing.RegisterRoute($"{nameof(WarehousesPage)}/{nameof(WarehouseDetailsPage)}/{nameof(ProductDetailsPage)}", typeof(ProductDetailsPage));
+            Routing.RegisterRoute($"{nameof(WarehousesPage)}/{nameof(WarehouseEditorPage)}", typeof(WarehouseEditorPage));
+            Routing.RegisterRoute($"{nameof(WarehousesPage)}/{nameof(WarehouseDetailsPage)}/{nameof(ProductEditorPage)}", typeof(ProductEditorPage));
         }
     }
 }
