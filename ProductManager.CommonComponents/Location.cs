af6e891dd559123b34cf3882bd1aedaf851b97d9
nemaproblem478 <danylosavchuk0102@gmail.com>
2026-04-19 15:15:34 +0300

    Add DisplayNames to Location Enums

--- ProductManager.CommonComponents/Location.cs
+++ ProductManager.CommonComponents/Location.cs
@@ -1,13 +1,19 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace ProductManager.CommonComponents
 {
     public enum Location
     {
+        [Display(Name = "Kyiv")]
         Kyiv,
+        [Display(Name = "Khmelnytsky")]
         Khmelnytsky,
+        [Display(Name = "Lviv")]
         Lviv,
+        [Display(Name = "Dnipro")]
         Dnipro,
+        [Display(Name = "Odesa")]
         Odesa
     }
 }
