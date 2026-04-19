336e3dce2a0d862b99be93721ac899001a85fdc3
nemaproblem478 <danylosavchuk0102@gmail.com>
2026-04-19 15:17:02 +0300

    Add StringToBoolConverter

---
+++ ProductManager/Tools/Converters/StringToBoolConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductManager.Tools.Converters
+{
+    public class StringToBoolConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return !string.IsNullOrWhiteSpace(value as string);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
